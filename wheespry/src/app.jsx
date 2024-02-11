dfb708d5e1602407aa1a24df883be16a8af3e794
Lamamba <contact@sanlamamba.com>
2024-02-11 19:34:13 +0100

    feat(initial) : adding tailwind, and antd

---
+++ wheespry/src/app.jsx
@@ -0,0 +1,14 @@
+import FullScreen from "./layout/FullScreen";
+
+export function App() {
+
+  return (
+    <FullScreen>
+        <button className="btn btn-primary">
+      hello
+    </button>
+    </FullScreen>
+  
+ 
+  )
+}
